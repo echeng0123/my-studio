a2552546802ec61534f5edb3a6025eac2f9593dd
Emily Cheng <echeng0123@gmail.com>
2023-09-08 11:11:06 -0400

    semi working genre junction search leave for now

--- client/src/components/GenreJunctions.jsx
+++ client/src/components/GenreJunctions.jsx
@@ -1,14 +1,20 @@
 // This component renders the genre junctions table
 
 import { useState, useEffect } from "react";
-import { fetchAllGenreJunc } from "../../fetching";
-import { fetchAllPhysInstr } from "../../fetching";
+import {
+	fetchAllGenreJunc,
+	fetchAllPhysInstr,
+	fetchAllVSTInstr,
+} from "../../fetching";
 
 export default function GenreJunctions() {
 	const [genreJuncs, setGenreJuncs] = useState([]);
 	const [physInstrs, setPhysInstrs] = useState([]);
+	const [VSTInstrs, setVSTInstrs] = useState([]);
 	const [searchParam, setSearchParam] = useState("");
-	const filteredJunctions = [];
+	const [searchParam2, setSearchParam2] = useState("");
+	const filteredPhysJunctions = [];
+	const filteredVSTJunctions = [];
 
 	// Pull in all genre junctions
 	useEffect(() => {
@@ -36,17 +42,43 @@ export default function GenreJunctions() {
 		getAllPhysInstr();
 	}, []);
 
+	// Pull in all virtual instruments
+	useEffect(() => {
+		async function getAllVSTInstr() {
+			const VSTResponse = await fetchAllVSTInstr();
+			if (VSTResponse) {
+				setVSTInstrs(VSTResponse);
+			} else {
+				console.error("Unable to fetch all virtual instruments");
+			}
+		}
+		getAllVSTInstr();
+	}, []);
+
 	// Matching the objects in the genre junction table with the ones in the all physical instruments table
 
 	genreJuncs.map((junc) => {
 		for (let i = 0; i < physInstrs.length; i++) {
 			if (physInstrs[i].phys_id == junc.physid) {
-				filteredJunctions.push(physInstrs[i]);
+				filteredPhysJunctions.push(physInstrs[i]);
 			}
 		}
-		return filteredJunctions;
+		return filteredPhysJunctions;
 	});
-	console.log(filteredJunctions);
+	console.log("FPJ", filteredPhysJunctions);
+
+	// Matching the objects in the genre junction table with the ones in the all virtual instruments table
+
+	console.log("GJ", genreJuncs);
+	genreJuncs.map((junc) => {
+		for (let i = 0; i < VSTInstrs.length; i++) {
+			if (VSTInstrs[i].VST_id == junc.vstid) {
+				filteredVSTJunctions.push(VSTInstrs[i]);
+			}
+		}
+		return filteredVSTJunctions;
+	});
+	console.log("FVJ", filteredVSTJunctions);
 
 	// converts instrument name to title case/sentence case for later display in rendering
 	function titleCase(str) {
@@ -58,9 +90,9 @@ export default function GenreJunctions() {
 	}
 
 	// allows for search functionality
-	// users can search any of the instrument parameters AND tags
+	// users can search any of the instrument parameters AND tags in phys instr table
 	const genreJuncToDisplay = searchParam
-		? filteredJunctions.filter(
+		? filteredPhysJunctions.filter(
 				(filteredJunc) =>
 					filteredJunc.instr_name.toLowerCase().includes(searchParam) ||
 					filteredJunc.instr_family.toLowerCase().includes(searchParam) ||
@@ -68,68 +100,73 @@ export default function GenreJunctions() {
 					filteredJunc.art_type.toLowerCase().includes(searchParam) ||
 					JSON.stringify(filteredJunc.tags).toLowerCase().includes(searchParam)
 		  )
-		: filteredJunctions;
-	console.log("genreJuncToDisplay", genreJuncToDisplay);
+		: filteredPhysJunctions;
+
+	// users can search any of the instrument parameters AND tags
+	const genreJuncToDisplay2 = searchParam
+		? filteredVSTJunctions.filter(
+				(filteredJunc) =>
+					filteredJunc.instr_name.toLowerCase().includes(searchParam) ||
+					filteredJunc.instr_family.toLowerCase().includes(searchParam) ||
+					filteredJunc.instr_category.toLowerCase().includes(searchParam) ||
+					filteredJunc.engine.toLowerCase().includes(searchParam) ||
+					filteredJunc.brand.toLowerCase().includes(searchParam) ||
+					JSON.stringify(filteredJunc.tags).toLowerCase().includes(searchParam)
+		  )
+		: filteredVSTJunctions;
 
 	return (
 		<div>
 			<h1>Genre Junctions</h1>
-			<div>
-				{genreJuncs.map((junc) => {
+			<h3>Search instruments for corresponding genres</h3>
+			<div id="search-phys-instr">
+				<label>
+					Search Physical Instruments:{" "}
+					<input
+						id="search-instr-bar"
+						type="text"
+						placeholder="Search instrument name, parameter, or tag"
+						onChange={(event) =>
+							setSearchParam(event.target.value.toLowerCase())
+						}
+					/>
+				</label>
+			</div>
+			<div id="search-phys-instr">
+				<label>
+					Search Virtual Instruments:{" "}
+					<input
+						id="search-instr-bar"
+						type="text"
+						placeholder="Search instrument name, parameter, or tag"
+						onChange={(event) =>
+							setSearchParam(event.target.value.toLowerCase())
+						}
+					/>
+				</label>
+			</div>
+			<div id="phys-juncs-container">
+				{genreJuncToDisplay.map((junc) => {
 					return (
 						// eslint-disable-next-line react/jsx-key
-						<div>
-							<h3>Genre Junction id: {junc.genrejunc_id}</h3>
+						<div id="physical-juncs">
+							{/* <h3>Genre Junction id: {junc.genrejunc_id}</h3> */}
 
-							{filteredJunctions.length > 0 ? (
-								<p>
-									Physical Instrument(s):{" "}
-									{titleCase(
-										filteredJunctions.filter(
-											(filterJunc) => filterJunc.phys_id == junc.physid
-										)[0].instr_name
-									)}
-								</p>
-							) : (
-								<></>
-							)}
+							<p>Physical Instrument(s): {titleCase(junc.instr_name)}</p>
+							<img src={junc.image_url} alt="" id="genre-images" />
+						</div>
+					);
+				})}
+			</div>
+			<div id="vst-juncs-container">
+				{genreJuncToDisplay2.map((junc) => {
+					return (
+						// eslint-disable-next-line react/jsx-key
+						<div id="physical-juncs">
+							{/* <h3>Genre Junction id: {junc.genrejunc_id}</h3> */}
 
-							<img
-								src={
-									filteredJunctions.length > 0
-										? filteredJunctions.filter(
-												(filterJunc) => filterJunc.phys_id == junc.physid
-										  )[0].image_url
-										: ""
-								}
-								alt=""
-								id="genre-images"
-							/>
-
-							{filteredJunctions.length > 0 ? (
-								<p>
-									Virtual Instrument(s):{" "}
-									{titleCase(
-										filteredJunctions.filter(
-											(filterJunc) => filterJunc.VST_id == junc.VSTid
-										)[0].instr_name
-									)}
-								</p>
-							) : (
-								<></>
-							)}
-
-							<img
-								src={
-									filteredJunctions.length > 0
-										? filteredJunctions.filter(
-												(filterJunc) => filterJunc.VST_id == junc.VSTid
-										  )[0].image_url
-										: ""
-								}
-								alt=""
-								id="genre-images"
-							/>
+							<p>Virtual Instrument(s): {titleCase(junc.instr_name)}</p>
+							<img src={junc.image_url} alt="" id="genre-images" />
 						</div>
 					);
 				})}
